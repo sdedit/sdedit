a380ae149f2bf72c7546c49380a646df139d289c
strauch <strauch@ca879305-32bc-4e4b-930d-0d5cde5c0db2>
2011-06-06 13:30:26 +0000

    experimental chain of responsibility for message processing

---
+++ QSD/branches/4.0-RC1-chain/src/net/sf/sdedit/diagram/dev/LifelineProcessor.java
@@ -0,0 +1,38 @@
+package net.sf.sdedit.diagram.dev;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.sdedit.diagram.Lifeline;
+import net.sf.sdedit.error.SemanticError;
+import net.sf.sdedit.error.SyntaxError;
+
+public class LifelineProcessor extends SequenceProcessor<Lifeline>{
+    
+    private final Map<String, Lifeline> lifelineMap;
+
+    protected LifelineProcessor(SequenceProcessorChain chain) {
+        super(chain);
+        lifelineMap = new HashMap<String,Lifeline>();
+    }
+
+    @Override
+    protected Class<Lifeline> getElementClass() {
+        return Lifeline.class;
+    }
+
+    @Override
+    public void processElement() throws SyntaxError, SemanticError {
+        Lifeline lifeline = getElement();
+        getChain().addDrawable(lifeline.getHead());
+        if (lifelineMap.containsKey(lifeline.getName())) {
+            raiseSyntaxError(lifeline.getName() + " already exists.");
+        }
+        if (!getDiagram().isThreaded() && lifeline.hasThread()) {
+            raiseSemanticError(lifeline.getName() + " cannot have its own thread when multithreading is not enabled");
+        }
+        getDiagram().addLifeline(lifeline);
+        
+    }
+
+}
