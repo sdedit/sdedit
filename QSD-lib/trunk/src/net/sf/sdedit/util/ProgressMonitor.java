85bbaaf6c7ee565336a7a83a7bfeb8e5359c0e39
vu2dev <vu2dev@ca879305-32bc-4e4b-930d-0d5cde5c0db2>
2013-02-13 07:28:07 +0000

    ProgressMonitor

---
+++ QSD-lib/trunk/src/net/sf/sdedit/util/ProgressMonitor.java
@@ -0,0 +1,7 @@
+package net.sf.sdedit.util;
+
+public interface ProgressMonitor {
+	
+	public void setProgress (int total, int done);
+
+}
---
+++ QSD-lib/trunk/src/net/sf/sdedit/util/ProgressMonitorable.java
@@ -0,0 +1,7 @@
+package net.sf.sdedit.util;
+
+public interface ProgressMonitorable {
+	
+	public void addProgressMonitor (ProgressMonitor progressMonitor);
+
+}
