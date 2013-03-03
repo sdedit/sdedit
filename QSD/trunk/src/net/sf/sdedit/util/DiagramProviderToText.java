ea63ac84a508ba275d066335c664aca2c375dcbd
strauch <strauch@ca879305-32bc-4e4b-930d-0d5cde5c0db2>
2013-03-03 19:20:17 +0000

    DiagramFactory

--- QSD/trunk/src/net/sf/sdedit/util/DiagramProviderToText.java
+++ QSD/trunk/src/net/sf/sdedit/util/DiagramProviderToText.java
@@ -24,13 +24,8 @@
 
 package net.sf.sdedit.util;
 
-import java.awt.Font;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 import net.sf.sdedit.config.Configuration;
 import net.sf.sdedit.config.ConfigurationManager;
@@ -39,10 +34,8 @@ import net.sf.sdedit.diagram.DiagramDataProvider;
 import net.sf.sdedit.diagram.IPaintDevice;
 import net.sf.sdedit.diagram.Lifeline;
 import net.sf.sdedit.diagram.MessageData;
-import net.sf.sdedit.drawable.Drawable;
-import net.sf.sdedit.drawable.Line;
+import net.sf.sdedit.diagram.NullPaintDevice;
 import net.sf.sdedit.drawable.Note;
-import net.sf.sdedit.drawable.SequenceElement;
 import net.sf.sdedit.error.DiagramError;
 import net.sf.sdedit.error.SyntaxError;
 
@@ -86,86 +79,7 @@ public class DiagramProviderToText implements DiagramDataProvider {
 
 	}
 
-	private static class NullPaintDevice implements IPaintDevice {
-
-		private Diagram diagram;
-
-		public void addOtherDrawable(Drawable drawable) {
-		}
-
-		public void addSequenceElement(SequenceElement elem) {
-		}
-
-		public void announce(int height) {
-		}
-
-		public void clear() {
-		}
-
-		public void close() {
-		}
-
-		public void computeAxes(int leftAxis) {
-		}
-
-		public void computeBounds() {
-		}
-
-		public Diagram getDiagram() {
-			return diagram;
-		}
-
-		public Font getFont(boolean bold) {
-			return Font.decode("Dialog");
-		}
-
-		public int getHeight() {
-			return 0;
-		}
-
-		public Line getRightBound() {
-			return null;
-		}
-
-		public int getTextHeight(boolean bold) {
-			return 0;
-		}
-
-		public int getTextHeight() {
-			return 0;
-		}
-
-		public int getTextWidth(String text, boolean bold) {
-			return 0;
-		}
-
-		public int getTextWidth(String text) {
-			return 0;
-		}
-
-		public int getWidth() {
-			return 0;
-		}
-
-		public boolean isEmpty() {
-			return true;
-		}
-
-		public void reinitialize() {
-		}
-
-		public void setDiagram(Diagram diagram) {
-			this.diagram = diagram;
-		}
-
-		public void writeToStream(OutputStream stream) throws IOException {
-		}
-
-		public Iterator<Drawable> iterator() {
-			return new LinkedList<Drawable>().iterator();
-		}
-
-	}
+	
 
 	public boolean advance() {
 		boolean flag = provider.advance();
