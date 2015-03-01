d4d3ac21b45c67624d4314a99c1feb4236709fed
sdedit <sdedit@github.com>
2015-03-01 12:51:20 +0100

    put path to jarfile in order

--- QSD/etc/test_javadoc.sh
+++ QSD/etc/test_javadoc.sh
@@ -6,8 +6,8 @@ cd src/main/java
 
 find . -type f -name "*.java" | xargs \
 javadoc \
- -tagletpath __dist/sdedit-jar/sdedit-4.2-SNAPSHOT.jar \
- -classpath __dist/sdedit-jar/sdedit-4.2-SNAPSHOT.jar \
+ -tagletpath ../../../__dist/sdedit-jar/sdedit-4.2-SNAPSHOT.jar \
+ -classpath ../../../__dist/sdedit-jar/sdedit-4.2-SNAPSHOT.jar \
  -taglet net.sf.sdedit.taglet.SequenceTaglet \
  -sourcepath . \
  -encoding utf-8 \
