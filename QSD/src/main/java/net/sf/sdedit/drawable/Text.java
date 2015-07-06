9749d78e086f83f007d3355161185987b30021d4
strauchm <strauchm@local>
2015-07-06 17:22:11 +0200

    drawMultilineString: center=false, underline=false

--- QSD/src/main/java/net/sf/sdedit/drawable/Text.java
+++ QSD/src/main/java/net/sf/sdedit/drawable/Text.java
@@ -58,7 +58,7 @@ public class Text extends Drawable
     protected void drawObject(Graphics2D g2d) {
         g2d.setFont(device.getFont(true));
         drawMultilineString(g2d, text, getLeft(), getBottom() -4 , 
-                simpleHeight, getWidth(), null);
+                simpleHeight, getWidth(), null, false, false);
     }
 
 }
