57f876dc6c7bd4218f390c9b03e9e8763ff29e01
strauch <markus-strauch@arcor.de>
2016-03-19 12:54:54 +0100

    use -subpackages instead of list of found files

--- QSD/etc/test_javadoc.sh
+++ QSD/etc/test_javadoc.sh
@@ -4,12 +4,12 @@ rm -rf doc
 
 jarfile=$(realpath __dist/sdedit-jar/sdedit*jar)
 
-find src/main/java ../QSD-lib/src/main/java -type f -name "*.java" | xargs \
 javadoc \
  -tagletpath $jarfile \
  -classpath $jarfile \
  -taglet net.sf.sdedit.taglet.SequenceTaglet \
  -sourcepath src/main/java:../QSD-lib/src/main/java \
+ -subpackages . \
  -encoding utf-8 \
  -docencoding utf-8 \
  -Xdoclint:none \
