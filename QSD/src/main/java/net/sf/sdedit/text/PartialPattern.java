1210cf058d8f84d48432ac662c45e38f7e12746f
strauch <markus-strauch@arcor.de>
2016-03-26 19:33:46 +0100

    read patterns from text file

--- QSD/src/main/java/net/sf/sdedit/text/PartialPattern.java
+++ QSD/src/main/java/net/sf/sdedit/text/PartialPattern.java
@@ -1,28 +1,51 @@
 package net.sf.sdedit.text;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.sdedit.util.Utilities;
+
 enum PartialPattern {
-	
-	LEVELS("(" + "\\[(\\d*),(\\d+)\\]" + "|"
-			+ "\\[(\\d+)\\]" + ")"),
-	
-	PREFIX("(\\(\\d*(,\\d+)?\\))?\\s*(.+)"),
-	
-	COLON("(?<!\\\\):(?!>)"),
-	
-	SPAWN("(?<!\\\\):>"),
-	
-	DOT("(?<!\\\\)\\."),
-	
-	EQ("(?<!\\\\)=");
-	
-	private String pattern;
-	
-	PartialPattern (String pattern) {
-		this.pattern = pattern;
+
+	LEVELS,
+
+	PREFIX,
+
+	COLON,
+
+	SPAWN,
+
+	DOT,
+
+	EQ;
+
+	private static Map<PartialPattern, String> patterns = new HashMap<PartialPattern, String>();
+
+	static {
+		URL resource = PartialPattern.class.getResource("patterns");
+		try {
+			for (String line : Utilities.readLines(resource.openStream(), Charset.forName("utf-8"))) {
+				line = line.trim();
+				if (!line.startsWith("#")) {
+					int i = line.indexOf('=');
+					if (i > 0) {
+						String key = line.substring(0, i);
+						String value = line.substring(i + 1);
+						patterns.put(PartialPattern.valueOf(key.trim()), value.trim());					
+					}					
+				}
+
+			}
+		} catch (IOException e) {
+			throw new IllegalStateException("cannot read patterns from " + resource);
+		}
 	}
-	
+
 	public String toString() {
-		return pattern;
+		return patterns.get(this);
 	}
-	
+
 }
