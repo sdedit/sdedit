b4ac335d43a9f7921ae656a66ef134da5cecc903
strauch <markus-strauch@arcor.de>
2016-05-01 11:19:50 +0200

    uncommented methods removed

--- QSD/src/main/java/com/sun/javadoc/ClassDoc.java
+++ QSD/src/main/java/com/sun/javadoc/ClassDoc.java
@@ -8,39 +8,13 @@ public interface ClassDoc {
 
 	public abstract boolean isExternalizable();
 
-	//public abstract MethodDoc[] serializationMethods();
-
-	//public abstract FieldDoc[] serializableFields();
-
 	public abstract boolean definesSerializableFields();
 
 	public abstract ClassDoc superclass();
 
-	//public abstract Type superclassType();
-
 	public abstract boolean subclassOf(ClassDoc paramClassDoc);
 
 	public abstract ClassDoc[] interfaces();
-
-	//public abstract Type[] interfaceTypes();
-
-	//public abstract TypeVariable[] typeParameters();
-
-	//public abstract ParamTag[] typeParamTags();
-
-	//public abstract FieldDoc[] fields();
-
-	//public abstract FieldDoc[] fields(boolean paramBoolean);
-
-	//public abstract FieldDoc[] enumConstants();
-
-	//public abstract MethodDoc[] methods();
-
-	//public abstract MethodDoc[] methods(boolean paramBoolean);
-
-	//public abstract ConstructorDoc[] constructors();
-
-	//public abstract ConstructorDoc[] constructors(boolean paramBoolean);
 
 	public abstract ClassDoc[] innerClasses();
 
