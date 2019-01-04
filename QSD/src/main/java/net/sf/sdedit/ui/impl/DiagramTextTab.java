9f7be0e8369f295be96015ac5a26bcf33ca134e6
Markus Strauch <markus-strauch@arcor.de>
2019-01-04 16:59:34 +0100

    copyright 2019

--- QSD/src/main/java/net/sf/sdedit/ui/impl/DiagramTextTab.java
+++ QSD/src/main/java/net/sf/sdedit/ui/impl/DiagramTextTab.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2006 - 2018, Markus Strauch.
+// Copyright (c) 2006 - 2019, Markus Strauch.
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
