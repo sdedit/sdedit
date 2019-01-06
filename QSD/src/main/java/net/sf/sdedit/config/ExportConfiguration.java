91d037ed1c0af25b21b7d32d05f9584d747b8db9
marku <marku@DESKTOP-ITS7S2E.fritz.box>
2019-01-06 11:25:37 +0100

    this is only for bitmap export; format and orientation omitted

--- QSD/src/main/java/net/sf/sdedit/config/ExportConfiguration.java
+++ QSD/src/main/java/net/sf/sdedit/config/ExportConfiguration.java
@@ -7,26 +7,15 @@ import net.sf.sdedit.ui.components.configuration.DataObject;
 
 public interface ExportConfiguration extends DataObject {
 
-	@Adjustable(editable = true, info = "File type", category = "Export", key = "1", choices = { "ps", "pdf", "emf",
-			"svg", "png", "gif", "jpg", "bmp" })
+	@Adjustable(editable = true, info = "Type", category = "File", key = "1", choices = {  "bmp", "emf",
+			 "gif", "jpg","png", "svg"}, isComboBoxEditable = false)
 	public void setType(String type);
 
 	public String getType();
 
-	public String getFormat();
-
-	@Adjustable(editable = true, info = "Page format", category = "Export", key = "2", choices = { "A0", "A1", "A2",
-			"A3", "A4", "A5", "A6" })
-	public void setFormat(String format);
-
-	public String getOrientation();
-	
-	@Adjustable(editable = true, info = "Orientation", category = "Export", key = "3", choices = { "Portrait", "Landscape"})
-	public void setOrientation(String orientation);
-
 	public File getFile();
 
-	@Adjustable(editable = true, info = "File", category = "Export", key = "4")
+	@Adjustable(editable = true, info = "Name", category = "File", key = "2")
 	public void setFile(File file);
 
 }
